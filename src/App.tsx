c9dadde6ece95ead9e64522ae5d9d7af4bc21f6a
Mikołaj Depta <mikdepta@gmail.com>
2025-07-22 03:41:54 +0200

    Init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-center">
+        Tailwind works!
+      </h1>
+    </>
+  );
+}
+
+export default App;
